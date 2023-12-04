8dd4b472f02b7542c595df39042ffa1e911fd834
Ahzam <Ahzamzobairi402@gmail.com>
2023-12-04 21:42:54 +0530

    To check if the given year is leap year or not

---
+++ C/Simple_logics/002.Leap_Year.c
@@ -0,0 +1,23 @@
+/*
+Write a program to find leap year
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int year;
+    printf("Enter Year : ");
+    scanf("%d", &year);
+
+    if ( (year % 4 ==0 && year % 100 !=0) || (year % 400 == 0))
+    {
+        printf("Leap Year \n");
+    }
+    else
+    {
+        printf("Not Leap Year \n");
+    }
+
+    return 0;
+}
